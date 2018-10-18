b204c71f00c1ebbfc82701c7e9b9e82904ba325d
saltizer <43050989+saltizer@users.noreply.github.com>
2018-10-18 18:19:04 -0400

    Update set_mpi_path.sh

--- set_mpi_path.sh
+++ set_mpi_path.sh
@@ -1,2 +1,2 @@
-echo "export PATH='$PATH:/software/openmpi/3.1.2/bin'" >> /users/ka837933/.bashrc
-echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/openmpi/3.1.2/lib/'" >> /users/ka837933/.bashrc
+echo "export PATH='$PATH:/software/openmpi/3.1.2/bin'" >> /users/sa834534/.bashrc
+echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/openmpi/3.1.2/lib/'" >> /users/sa834534/.bashrc
